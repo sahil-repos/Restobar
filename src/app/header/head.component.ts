e835bf77aa56a8f18d30afadcce392c3bab70ef9
Mohammed Sahil <mohammed.sahil@nagarro.com>
2020-02-10 12:35:26 +0530

    using componenets

--- src/app/header/head.component.ts
+++ src/app/header/head.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'app-header',
-    templateURL:'./header.component.html',
+    templateUrl:'./header.component.html',
     
 })
 export class HeaderComponent{
