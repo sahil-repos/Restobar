77bc35f9fe648f2715721448b64a741a92777410
Mohammed Sahil <mohammed.sahil@nagarro.com>
2020-02-10 11:48:35 +0530

    first comm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from 'src/app/header/head.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListsComponent } from './recipes/recipe-lists/recipe-lists.component';
+import { RecipeDetailsComponent } from './recipes/recipe-lists/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListsComponent,
+    RecipeDetailsComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/head.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-header',
+    templateURL:'./header.component.html',
+    
+})
+export class HeaderComponent{
+
+}
