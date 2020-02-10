ad7f5599a0fcfb162bd891ed9c621f3e206eec07
Mohammed Sahil <mohammed.sahil@nagarro.com>
2020-02-10 12:01:53 +0530

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/app/header/head.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListsComponent } from './recipes/recipe-lists/recipe-lists.component';
-import { RecipeDetailsComponent } from './recipes/recipe-lists/recipe-details/recipe-details.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './recipes/recipe-lists/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,9 @@ import { RecipeDetailsComponent } from './recipes/recipe-lists/recipe-details/re
     RecipesComponent,
     RecipeListsComponent,
     RecipeDetailsComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule
